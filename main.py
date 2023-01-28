333021be87d913fb4aa148b71d2333512158dd82
xareyli <xareyli@gmail.com>
2023-01-28 11:11:35 +0300

    Handle home

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ app = FastAPI()
 @app.post("/webhook", status_code=http.HTTPStatus.ACCEPTED)
 async def webhook(request: Request):
     return json.dumps(request)
+
+
+@app.get("/", status_code=http.HTTPStatus.ACCEPTED)
+async def home(request: Request):
+    return json.dumps('hey')
