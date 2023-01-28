81ae948d602e2f5579fad0f018aa9f3cf12dfa97
xareyli <xareyli@gmail.com>
2023-01-28 11:06:50 +0300

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+from os import getenv
+from fastapi import FastAPI, Request, Response
+import http
+import json
+
+
+app = FastAPI()
+
+@app.post("/webhook", status_code=http.HTTPStatus.ACCEPTED)
+async def webhook(request: Request):
+    return json.dumps(request)
